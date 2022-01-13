e39a2d793422e55b6d4c55ac7bd6d81c19f713a2
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2022-01-13 22:45:26 +0300

    add notify to create cvs selected

--- app/jobs/create_csv_selected_job.rb
+++ app/jobs/create_csv_selected_job.rb
@@ -2,6 +2,8 @@ class CreateCsvSelectedJob < ApplicationJob
   queue_as :default
 
   def perform(search_id_by_q)
+    ActionCable.server.broadcast 'status_process', {distributor: "product", process: "create_csv_selected", status: "start", message: "Создание Csv Selected"}
     Services::CsvSelected.call(search_id_by_q)
+    ActionCable.server.broadcast 'status_process', {distributor: "product", process: "create_csv_selected", status: "finish", message: "Создание Csv Selected"}
   end
 end
