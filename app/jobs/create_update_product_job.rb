9b17002c48ba1fde1e6beee0549939a05c073c18
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2022-06-14 04:40:51 +0300

    change order run task update distributor

--- app/jobs/create_update_product_job.rb
+++ app/jobs/create_update_product_job.rb
@@ -2,10 +2,10 @@ class CreateUpdateProductJob < ApplicationJob
   queue_as :default
 
   def perform
-    Services::GettingProductDistributer::Elevel.new.call
     Services::GettingProductDistributer::Maytoni.call
     Services::GettingProductDistributer::Mantra.call
     Services::GettingProductDistributer::Lightstar.call
     Services::GettingProductDistributer::Swg.call
+    Services::GettingProductDistributer::Elevel.new.call rescue retry
   end
 end
